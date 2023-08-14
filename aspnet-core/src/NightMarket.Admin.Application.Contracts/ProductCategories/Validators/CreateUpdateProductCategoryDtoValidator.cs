60e445bf6bcebaf6b1ca06fe45b98c8a1eb2df07
NguyenVanVuong <hayasilver123@gmail.com>
2023-08-14 17:27:16 +0700

    Feature : Apply fluent validation module for dto

---
+++ aspnet-core/src/NightMarket.Admin.Application.Contracts/ProductCategories/Validators/CreateUpdateProductCategoryDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using NightMarket.Admin.ProductCategories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NightMarket.Admin.ProductCategories.Validators
+{
+    public class CreateUpdateProductCategoryDtoValidator : AbstractValidator<CreateUpdateProductCategoryDto>
+    {
+        public CreateUpdateProductCategoryDtoValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(50);
+            RuleFor(x => x.Slug).NotEmpty().MaximumLength(50);
+            RuleFor(x => x.Code).NotEmpty().MaximumLength(50);
+            RuleFor(x => x.CoverPicture).MaximumLength(250);
+            RuleFor(x => x.SeoMetaDescription).NotEmpty().MaximumLength(250);
+
+        }
+    }
+}
