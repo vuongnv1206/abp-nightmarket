78e2b94450d44b95f5fd1afc9fb5788f8205691b
NguyenVanVuong <hayasilver123@gmail.com>
2023-08-18 17:24:11 +0700

    Feature : [BE]Check duplicate data + [FE]Show notification error

--- aspnet-core/src/NightMarket.Admin.Application/Products/ProductAppService.cs
+++ aspnet-core/src/NightMarket.Admin.Application/Products/ProductAppService.cs
@@ -1,11 +1,14 @@
 ﻿using NightMarket.Admin.Commons;
 using NightMarket.ProductCategories;
 using NightMarket.Products;
+using NightMarket.Products.DomainServices;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
@@ -22,11 +25,64 @@ namespace NightMarket.Admin.Products
             CreateUpdateProductDto>,
         IProductAppService
     {
-        public ProductAppService(IRepository<Product, Guid> repository) : base(repository)
+
+        private readonly ProductManager _productManager;
+        private readonly IRepository<ProductCategory,Guid> _categoryRepository;
+        public ProductAppService(IRepository<Product, Guid> repository,
+            ProductManager productManager,
+			IRepository<ProductCategory, Guid> categoryRepository) : base(repository)
         {
+            _productManager = productManager;
+            _categoryRepository = categoryRepository;
         }
 
-		
+		public override async Task<ProductDto> CreateAsync(CreateUpdateProductDto input)
+		{
+            var product = await _productManager.CreateAsync(input.ManufacturerId, input.Name, input.Code,
+                input.Slug, input.ProductType, input.SKU, input.SortOrder, input.Visibility, input.IsActive, input.CategoryId,
+                input.SeoMetaDescription, input.Description, input.ThumbnailPicture, input.SellPrice
+                );
+
+            var result = await Repository.InsertAsync(product );
+            return ObjectMapper.Map<Product, ProductDto>( result );
+		}
+
+		public override async Task<ProductDto> UpdateAsync(Guid id, CreateUpdateProductDto input)
+		{
+			var product = await Repository.GetAsync( id );
+            if ( product == null )
+			{
+                throw new UserFriendlyException($"There product is not exist with this id: { id }",NightMarketDomainErrorCodes.ProductIsNotExists).WithData("id",id);
+            }
+            product.ManufacturerId = input.ManufacturerId;
+            if ( product.Name != input.Name)
+            {
+                await _productManager.ChangeNameAsync(product,input.Name);
+            }
+            product.Code = input.Code;
+            product.Slug = input.Slug;
+            product.ProductType = input.ProductType;
+            product.SKU = input.SKU;
+            product.SortOrder = input.SortOrder;
+            product.Visibility = input.Visibility;
+            product.IsActive = input.IsActive;
+            if(product.CategoryId != input.CategoryId)  
+            {
+				product.CategoryId = input.CategoryId;
+                var category = await _categoryRepository.GetAsync( input.CategoryId );
+                product.CategoryName = category.Name;
+                product.CategorySlug = category.Slug;
+			}
+            product.SeoMetaDescription = input.SeoMetaDescription;
+            product.Description = input.Description;
+            product.ThumbnailPicture = input.ThumbnailPicture;
+            product.SellPrice = input.SellPrice;
+
+            await Repository.UpdateAsync( product );
+			return ObjectMapper.Map<Product, ProductDto>(product);
+		}
+
+
 
 		public async Task DeleteMultipleAsync(IEnumerable<Guid> ids)
         {
--- aspnet-core/src/NightMarket.Domain.Shared/NightMarketDomainErrorCodes.cs
+++ aspnet-core/src/NightMarket.Domain.Shared/NightMarketDomainErrorCodes.cs
@@ -2,5 +2,10 @@
 
 public static class NightMarketDomainErrorCodes
 {
-    /* You can add your business exception error codes here, as constants */
+	/* You can add your business exception error codes here, as constants */
+
+	public const string ProductNameAlreadyExists = "NightMarket:00001";
+	public const string ProductCodeAlreadyExists = "NightMarket:00002";
+	public const string ProductSKUAlreadyExists = "NightMarket:00003";
+	public const string ProductIsNotExists = "NightMarket:00004";
 }
