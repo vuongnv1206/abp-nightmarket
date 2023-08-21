7a3438f18c685f9eb6788c0a8610c3750424c781
NguyenVanVuong <hayasilver123@gmail.com>
2023-08-21 16:20:43 +0700

    Feature : [BE]Crud & validation ProductAttribute

---
+++ aspnet-core/src/NightMarket.Admin.Application.Contracts/ProductAttributes/Validators/CreateUpdateProductAttributeDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NightMarket.Admin.ProductAttributes.Validators
+{
+	public class CreateUpdateProductAttributeDtoValidator :  AbstractValidator<CreateUpdateProductAttributeDto>
+	{
+        public CreateUpdateProductAttributeDtoValidator()
+        {
+			RuleFor(x => x.Label).NotEmpty().MaximumLength(50);
+			RuleFor(x => x.Code).NotEmpty().MaximumLength(50);
+			RuleFor(x => x.DataType).NotNull();
+			//RuleFor(x => x.SeoMetaDescription).NotEmpty().MaximumLength(250);
+		}
+    }
+}
